96b5c58bd22d894934202cde79ef80234c81a84d
Nathan Lee <54863927+nathan-jimin@users.noreply.github.com>
2021-11-01 12:00:47 -0700

    Changed Attack Hotkey
    Changed attack button from left click to spacebar
    Added rough visual indicator of attack range using blank rectangle

--- Assets/Player/PlayerAttack.cs
+++ Assets/Player/PlayerAttack.cs
@@ -23,8 +23,9 @@ public class PlayerAttack : MonoBehaviour
     void Update()
     {
         if (attackTime <= 0) {
-            if (Input.GetButton("Fire1")) {
+            if (Input.GetButton("Jump")) {
                 // deal damage to all enemies within attack range
+                print("space button pressed");
                 Collider2D[] damage = Physics2D.OverlapCircleAll(attackLocation.position, attackRange, enemies);
 
                 for (int i = 0; i < damage.Length; i++) {
