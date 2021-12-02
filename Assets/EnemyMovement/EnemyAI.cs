49c62334edb2b0de2e58ce773384c325b7df3133
TheAmitBarua <89492966+TheAmitBarua@users.noreply.github.com>
2021-12-02 12:39:18 -0800

    Update EnemyAI.cs

--- Assets/EnemyMovement/EnemyAI.cs
+++ Assets/EnemyMovement/EnemyAI.cs
@@ -87,4 +87,44 @@ public class EnemyAI : MonoBehaviour
          {
  
          }
+         
+ public class EnemyAttack : MonoBehaviour
+ {
+ 
+     public float enemyCooldown = 1;
+     public float damage = 1;
+ 
+     private bool playerInRange = false;
+     private bool canAttack = true;
+ 
+     private void Update()
+     {
+         if (playerInRange && canAttack)
+         {
+             GameObject.Find("Player").GetComponent<ControllerForPlayer>().currentHealth -= damage;
+             StartCoroutine(AttackCooldown());
+         }
+     }
+ 
+     void OnTriggerEnter(Collider other)
+     {
+         if (other.gameObject.CompareTag("Player"));
+          {
+             playerInRange = true;
+         }
+     }
+     void OnTriggerExit(Collider other)
+     {
+         if (other.gameObject.CompareTag("Player"));
+          {
+             playerInRange = false;
+         }
+     }
+     IEnumerator AttackCooldown()
+     {
+         canAttack = false;
+         yield return new WaitForSeconds(enemyCooldown);
+         canAttack = true;
+     }
+ }
  }
