0987793f81c72f722c0df4ec0e3a427f47d68a88
Nathan Lee <54863927+nathan-jimin@users.noreply.github.com>
2021-12-08 13:06:43 -0800

    Fixed Pause Bug

--- Assets/Screen Transitions/Pause.cs
+++ Assets/Screen Transitions/Pause.cs
@@ -7,18 +7,22 @@ public class Pause : MonoBehaviour
 {
     public bool _paused = false;
 
+    public SpriteRenderer img;
+
     public void pauseGame()
     {
         if(_paused)
         {
             Time.timeScale = 1;
             _paused = false;
+            img.color = new Color (1, 1, 1, 0);
         }
         else
         {
             Time.timeScale = 0;
             _paused = true;
             //SceneManager.LoadScene(5);
+            img.color = new Color (1, 1, 1, 255);
         }
     }
 }
